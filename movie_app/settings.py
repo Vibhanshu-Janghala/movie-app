6026eed0ef22bf1df0ffb88227139154e5e45c7e
Vibhanshu-Janghala <vibhanshujanghala1@gmail.com>
2021-07-07 00:25:04 +0530

    fixing access error

--- movie_app/settings.py
+++ movie_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
